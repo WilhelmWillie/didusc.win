9e7c310db530a8f354b09723602047a2af6e2b17
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-09-03 15:29:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Did USC Win?
+---
+Simple site that checks whether or not USC won their last football game.
+
+See live: https://didusc.win 
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Did USC Win?</title>
+      </Head>
+
+      Did USC win?
+    </>
+  );
+};
+
+export default Home;
