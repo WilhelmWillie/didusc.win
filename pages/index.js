dede8bd26560ac25c0eb03c7f9e5ac06a7a5fd6a
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-09-03 15:47:12 -0700

    Added logic to show message if USC won a game or not

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from "next/head";
 import fetch from "isomorphic-unfetch";
 
+const didUSCWin= (away, home) => {
+  if (away.team === 'USC') {
+    return away.points > home.points;
+  }
+
+  return home.points > away.points;
+}
+
 const Home = ({ data }) => {
   const games = data.map(game => (
     <div key={game.id}>
@@ -14,6 +22,21 @@ const Home = ({ data }) => {
           <b>{game.home_team}: </b> {game.home_points}
           <br/>
           <b>{game.away_team}: </b> {game.away_points}
+
+          <br /><br/>
+
+          {
+            didUSCWin(
+              {
+                team: game.away_team, 
+                points: game.away_points,
+              }, 
+              {
+                team: game.home_team,
+                points: game.home_points,
+              }
+            ) ? <b>Yes, USC won.</b> : <b>No, USC lost.</b>
+          }
         </>
       ) : null}
 
