42808df236dbd78fb12462ad35cb327ce33f5bcd
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-09-03 15:39:16 -0700

    Home page fetches data from API and displays basic game data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
 import Head from "next/head";
+import fetch from "isomorphic-unfetch";
 
-const Home = () => {
+const Home = ({ data }) => {
+  const games = data.map(game => (
+    <div key={game.id}>
+      <b>Home:</b> {game.home_team}
+      <br/>
+      <b>Away:</b> {game.away_team}
+      <br/>
+
+      {game.home_points && game.away_points ? (
+        <>
+          <b>{game.home_team}: </b> {game.home_points}
+          <br/>
+          <b>{game.away_team}: </b> {game.away_points}
+        </>
+      ) : null}
+
+      <hr/>
+    </div>
+  ));
+
   return (
     <>
       <Head>
@@ -8,8 +28,19 @@ const Home = () => {
       </Head>
 
       Did USC win?
+
+      <hr/>
+
+      {games}
     </>
   );
 };
+
+Home.getInitialProps = async () => {
+  const res = await fetch('https://api.collegefootballdata.com/games?year=2019&seasonType=regular&team=USC')
+  const data = await res.json();
+
+  return { data };
+}
 
 export default Home;
