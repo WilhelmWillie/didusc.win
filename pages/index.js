69498e2bdf2c193dc7c4f3096421eaffa4f47743
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-09-03 15:53:12 -0700

    Prettier :)

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,45 @@
 import Head from "next/head";
 import fetch from "isomorphic-unfetch";
 
-const didUSCWin= (away, home) => {
-  if (away.team === 'USC') {
+const didUSCWin = (away, home) => {
+  if (away.team === "USC") {
     return away.points > home.points;
   }
 
   return home.points > away.points;
-}
+};
 
 const Home = ({ data }) => {
   const games = data.map(game => (
     <div key={game.id}>
       <b>Home:</b> {game.home_team}
-      <br/>
+      <br />
       <b>Away:</b> {game.away_team}
-      <br/>
-
+      <br />
       {game.home_points && game.away_points ? (
         <>
           <b>{game.home_team}: </b> {game.home_points}
-          <br/>
+          <br />
           <b>{game.away_team}: </b> {game.away_points}
-
-          <br /><br/>
-
-          {
-            didUSCWin(
-              {
-                team: game.away_team, 
-                points: game.away_points,
-              }, 
-              {
-                team: game.home_team,
-                points: game.home_points,
-              }
-            ) ? <b>Yes, USC won.</b> : <b>No, USC lost.</b>
-          }
+          <br />
+          <br />
+          {didUSCWin(
+            {
+              team: game.away_team,
+              points: game.away_points
+            },
+            {
+              team: game.home_team,
+              points: game.home_points
+            }
+          ) ? (
+            <b>Yes, USC won.</b>
+          ) : (
+            <b>No, USC lost.</b>
+          )}
         </>
       ) : null}
-
-      <hr/>
+      <hr />
     </div>
   ));
 
@@ -49,21 +48,20 @@ const Home = ({ data }) => {
       <Head>
         <title>Did USC Win?</title>
       </Head>
-
       Did USC win?
-
-      <hr/>
-
+      <hr />
       {games}
     </>
   );
 };
 
 Home.getInitialProps = async () => {
-  const res = await fetch('https://api.collegefootballdata.com/games?year=2019&seasonType=regular&team=USC')
+  const res = await fetch(
+    "https://api.collegefootballdata.com/games?year=2019&seasonType=regular&team=USC"
+  );
   const data = await res.json();
 
   return { data };
-}
+};
 
 export default Home;
