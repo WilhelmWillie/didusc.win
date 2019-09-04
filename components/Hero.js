c6054b346b6c79a954c43eafd513e073ad19e979
Wilhelm Willie <wilhelmwillie@gmail.com>
2019-09-04 10:17:01 -0700

    More responsive Hero

--- components/Hero.js
+++ components/Hero.js
@@ -7,8 +7,10 @@ const Hero = ({ didWin }) => {
     <>
       <Wrapper>
         <Container>
-          <Question>Did USC win?</Question>
-          <Answer>{didWin ? "Yes" : "No"}</Answer>
+          <Column>
+            <Question>Did USC win?</Question>
+            <Answer>{didWin ? "Yes" : "No"}</Answer>
+          </Column>
         </Container>
       </Wrapper>
     </>
@@ -19,6 +21,21 @@ const Wrapper = styled.div`
   background-image: linear-gradient(52deg, #3e0c0c 0%, #e73b61 100%);
   padding-top: 80px;
   padding-bottom: 80px;
+
+  ${({ theme }) => theme.media.tablet`
+    padding-top: 60px;
+    padding-bottom:  60px;
+    text-align: center;
+  `};
+`;
+
+const Column = styled.div`
+  width: 50%;
+  text-align: center;
+
+  ${({ theme }) => theme.media.tablet`
+    width: 100%;
+  `};
 `;
 
 const Question = styled.h1`
@@ -28,6 +45,10 @@ const Question = styled.h1`
   color: transparent;
   -webkit-text-stroke-width: 1px;
   -webkit-text-stroke-color: ${({ theme }) => theme.color.white};
+
+  ${({ theme }) => theme.media.tablet`
+    font-size: 32px;
+  `};
 `;
 
 const Answer = styled.h2`
@@ -35,6 +56,30 @@ const Answer = styled.h2`
   font-size: ${({ theme }) => theme.fontSize.max};
   font-weight: 800;
   text-transform: uppercase;
+  margin-top: 16px;
+  width: auto;
+
+  &:before {
+    display: block;
+    content: "";
+    height: 2px;
+    margin: 0 auto 8px;
+    width: 80px;
+    background-color: ${({ theme }) => theme.color.gold};
+  }
+
+  &:after {
+    display: block;
+    content: "";
+    height: 2px;
+    margin: 8px auto 0;
+    width: 80px;
+    background-color: ${({ theme }) => theme.color.gold};
+  }
+
+  ${({ theme }) => theme.media.tablet`
+    font-size: 60px;
+  `};
 `;
 
 export default Hero;
